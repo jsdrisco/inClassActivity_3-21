09cdaea42114c929d8ef9129dec9605685c14508
jsdrisco <112837177+jsdrisco@users.noreply.github.com>
2023-03-21 16:43:01 -0400

    Added documentation

--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -1,7 +1,13 @@
+
 def remove_duplicates(arr):
+    # Sort the input array
+    # Create arrays that will contain unique elements, removed elements
     arr.sort()
     unique_arr = []
     removed_elements = []
+    
+    # Check if current element is identical to previous element
+    # If unique, append to unique_arr, otherwise append to removed_elements
     for i in range(len(arr)):
         if i == 0 or arr[i] != arr[i-1]:
             unique_arr.append(arr[i])
